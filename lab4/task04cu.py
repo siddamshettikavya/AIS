b21b85a2bb4f4850467d3fdc149f92b8cfa76369
siddamshettikavya <2403a51037@sru.edu.in>
2025-08-13 20:06:35 +0530

    Delete lab4 directory

--- lab4/task04cu.py
+++
@@ -1,6 +0,0 @@
-def count_vowels(s):
-    vowels = "aeiouAEIOU"
-    return sum(1 for char in s if char in vowels)
-print(count_vowels("hello"))   
-print(count_vowels("OpenAI"))  
-print(count_vowels("sky"))     
